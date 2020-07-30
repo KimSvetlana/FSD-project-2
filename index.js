7c112aafcb7335af498af0b04ed205415c525453
KimSvetlana <svetlanamingalieva@gmail.com>
2020-07-30 15:53:14 +0700

    улучшение кода

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
             expansion: '100%',
             thickness: 8,
             vertical: false,
+            indicatorVisibility: false,
+            slideHandler: null,
         }, options);
 
         const makeSliderFunction = function(){
@@ -17,6 +19,18 @@
             
             $($this).append(content);
 
+            let _handleMovedHandlers = [];
+            if (options.slideHandler != null)
+                _handleMovedHandlers.push(options.slideHandler);
+
+            const handleMovedEvent = function(sliderCenterPos, sliderRadius){
+                _handleMovedHandlers.forEach(handler => {
+                    handler(sliderCenterPos, sliderRadius);
+                })
+            };
+
+
+
             
             let slider =$this.find('.slider-range-content');
             let handle =$this.find('.slider-range-handle');
@@ -25,68 +39,113 @@
             let sliderValue = $this.find('#slider-range-value');
 
             // определяем ориентацию слайдера
-            let length = '';
-            let thickness = '';
-            let handlePos = '';
-
-            if (options.vertical){
-                length = 'height';
-                thickness = 'width';
-                handlePos = 'left'
+            let _leftPropertyName;
+            let _widthPropertyName = '';
+            let _thicknessPropertyName = '';
+            let _topPropertyName = '';
+            let _handleWidth = options.thickness * 2;
+            let _handleHeight = options.thickness * 2;
+            if(!options.vertical){
+                _leftPropertyName = 'left';
+                _widthPropertyName = 'width';
+                _thicknessPropertyName = 'height';
+                _topPropertyName = 'top';
             }
             else{
-                length = 'width';
-                thickness = 'height';
-                handlePos = 'top';
+                _leftPropertyName = 'top';
+                _widthPropertyName = 'height';
+                _thicknessPropertyName = 'width';
+                _topPropertyName = 'left'
             }
-
+            
+            // параметры и размеры слайдера
             colorRange.css('background-color', options.backgroundColor);
-            slider.css(thickness, options.thickness);
-            colorRange.css(thickness, options.thickness);
-            slider.css(length, options.expansion);
+            slider.css(_thicknessPropertyName, options.thickness);
+            colorRange.css(_thicknessPropertyName, options.thickness);
+            slider.css(_widthPropertyName, options.expansion);
 
             // параметры и расположение рукоятки
-            handle.css({'width': 2*options.thickness, 'height': 2*options.thickness});
-           
+            handle.css({'width': _handleWidth, 'height': _handleHeight});
+
+            // зная все размеры, можно уточнить границы перемещения рукоятки
+            let _minPos;
+            let _maxPos;
+            if(!options.vertical){
+                _minPos = slider.offset().left;
+                _maxPos = _minPos + slider.width() - _handleWidth;
+            }
+            else{
+                _minPos = slider.offset().top;
+                _maxPos = _minPos + slider.height() - _handleHeight;
+            }
+
+            // выравнивание рукоятки относительно кулисы
             let handlePosObject = {};
-            handlePosObject[handlePos] = -options.thickness/2;
+            handlePosObject[_topPropertyName] = -options.thickness/2;
             handle.css(handlePosObject);
+
+            let indicatorContent = "<span class='indicator'>0</span>";
+            if(options.indicatorVisibility){
+                slider.append(indicatorContent);
+            }
+            let indicator = $this.find('.indicator');
+            
 
             // функция работы слайдера
             const onHandleMove = (movePosition) => {
-                let minPos;
-                let maxPos;
                 let movePos;
-                let offsetProperty;
                 if(options.vertical){
-                    minPos = slider.offset().top;
-                    maxPos = minPos + slider.height();
-                    movePos = movePosition.pageY;
-                    offsetProperty = 'top';
+                    movePos = movePosition.pageY - _handleHeight / 2;
                 }
                 else{
-                    minPos = slider.offset().left;
-                    maxPos = minPos + slider.width();
-                    movePos = movePosition.pageX;
-                    offsetProperty = 'left';
+                    movePos = movePosition.pageX - _handleWidth / 2;
                 }
                 
-                if(movePos < minPos){
-                    movePos = minPos;
+                if(movePos < _minPos){
+                    movePos = _minPos;
                 }
-                else if(movePos > maxPos){
-                    movePos = maxPos;
+                else if(movePos > _maxPos){
+                    movePos = _maxPos;
                 }
 
                 let offsetModifier = {};
-                offsetModifier[offsetProperty] = movePos;
+                offsetModifier[_leftPropertyName] = movePos;
                 handle.offset(offsetModifier);
 
-                // закрашиваем слайдер цветом
-                colorRange.css(length, movePos - minPos);
+                indicator.offset(offsetModifier);
 
-                sliderValue.text(movePos - minPos);
+                
+                let textValue = movePos - _minPos;
+                if(movePos === _maxPos){
+                    sliderValue.text(textValue + 2 * options.thickness);
+                    indicator.text(textValue + 2 * options.thickness);
+                }
+                else{
+                    sliderValue.text(textValue);
+                    indicator.text(textValue);
+                }
+                
+                // вызываем событие перемещкния(передаем координату центра и радиус бегунка )
+                handleMovedEvent(movePos + _handleWidth / 2, _handleWidth / 2);
             };
+            
+            // Обработать перемещение бегунка
+            const onHandleMoved = function(handleCenterPosition, handleRad){
+
+                // При перемещении надо закрасить слайдер цветом
+                applyColorToSlider(_minPos, handleCenterPosition);
+            };
+
+            // Закрасить слайдер
+            const applyColorToSlider = function(from, to){
+                let colorWidth = to - from;
+                let offSetObject = {_offsetPropertyName: from + _minPos};
+                colorRange.css(offSetObject);
+                colorRange.css(_widthPropertyName, colorWidth);
+            };
+
+
+            _handleMovedHandlers.push(onHandleMoved);
 
             const onHandleMouseDown  = () => {
                 $(document).on('mousemove', onHandleMove);
@@ -106,8 +165,8 @@
 
 })(jQuery);
 
-$('.slider-range-container').mySliderPlugin();
-$('.vertical-slider').mySliderPlugin({vertical:true, thickness: 15});
+$('.slider-range-container').mySliderPlugin({indicatorVisibility: true});
+$('.vertical-slider').mySliderPlugin({vertical:true, thickness: 15, indicatorVisibility: true});
 
 
 
